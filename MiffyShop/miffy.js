5bb9744984751d7dcfd8fdcc4724927c0432a069
jihyeryu <bonnie.jr54@gmail.com>
2020-10-06 17:28:50 +0900

    ing

---
+++ MiffyShop/miffy.js
@@ -0,0 +1,6 @@
+const miffyLogo = document.getElementById('img-logo');
+
+setTimeout(function () {
+    miffyLogo.classList.remove('miffy-invisiable');
+    miffyLogo.classList.add('miffy-visiable');
+},10);
